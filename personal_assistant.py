533250257048186d2c0a429d7a84982bc35d96fb
ge-kon <evilvolfy.of@gmail.com>
2024-11-29 19:15:16 +0300

    Added class Note and some methods for it

--- personal_assistant.py
+++ personal_assistant.py
@@ -10,16 +10,16 @@ def greetings():
     print('Добро пожаловать в Персональный помощник!')
 
 def show_main_menu():
-    sections = ['Управление заметками', 
-                'Управление задачами', 
-                'Управление контактами', 
-                'Управление финансовыми записями', 
-                'Калькулятор', 
-                'Выход']
+    sections = ['1. Управление заметками', 
+                '2. Управление задачами', 
+                '3. Управление контактами', 
+                '4. Управление финансовыми записями', 
+                '5. Калькулятор', 
+                '6. Выход']
     
     print('Выберите действие:')
-    for i in range(1, len(sections)+1):
-        print(f'{i}. {sections[i-1]}')
+    for i in sections:
+        print(i)
 
     while True:
         try:
@@ -32,23 +32,23 @@ def show_main_menu():
             print('Неверный ввод. Повторите ввод.')
 
 def show_notes_menu():
-    sections = ['Создание новой заметки', 
-                'Просмотр списка заметок', 
-                'Просмотр подробностей заметки', 
-                'Редактирование заметки', 
-                'Удаление заметки', 
-                'Импорт заметок в формате csv',
-                'Экспорт заметок в формате csv',
-                'Выход в главное меню']
+    sections = ['1. Создание новой заметки', 
+                '2. Просмотр списка заметок', 
+                '3. Просмотр подробностей заметки', 
+                '4. Редактирование заметки', 
+                '5. Удаление заметки', 
+                '6. Импорт заметок в формате csv',
+                '7. Экспорт заметок в формате csv',
+                '8. Выход в главное меню']
     
     print('Выберите действие:')
-    for i in range(1, len(sections)+1):
-        print(f'{i}. {sections[i-1]}')
+    for i in sections:
+        print(i)
 
     while True:
         try:
             c = int(input('>> '))
-            if 1 <= c <= 6:
+            if 1 <= c <= 7:
                 return c
             else:
                 raise ValueError    
@@ -94,13 +94,49 @@ def view_notes():
         for note in notes:
             print(f'Заголовок: {note.title} (id: {note.id}, дата: {note.timestamp})')
 
+#Просмотр определенной заметки
+def view_note():
+    notes = get_notes()
+    if len(notes) == 0:
+        print('Заметки отсутствуют')
+    else:
+        try:
+            id = int(input('Введите id заметки >> '))
+            if str(id) not in [note.id for note in notes]:
+                print('Заметка не найдена')
+            else:
+                for note in notes:
+                    if note.id == id:
+                        print(f'Заголовок: {note.title}\nСодержание: {note.content}\nДата: {note.timestamp}')
+                        break
+        except:
+            print('Некорректный id.')
+
+#Обновляем заметку
+def update_note():
+    notes = get_notes()
+    if len(notes) == 0:
+        print('Заметки отсутствуют')
+    else:
+        try:
+            id = int(input('Введите id заметки >> '))
+            if str(id) not in [note.id for note in notes]:
+                print('Заметка не найдена')
+            else:
+                tmp_title = input('Введи новый заголовок >> ')
+                tmp_content = input('Введи новое содержание >> ')
+                notes = [note for note in notes if note.id != str(id)]
+                notes.append(Note(id = str(id), title=tmp_title, content=tmp_content, timestamp=dt.now().strftime('%d-%m-%Y %H:%M:%S')))
+        except:
+            print('Некорректный id.')
+
 #Выбираем свободный id
 def get_free_id():
     notes = get_notes()
     ids = [int(note.id) for note in notes]
     return str(max(ids) + 1)
 
-#добавляем новую заметку
+#Добавляем новую заметку
 def add_note():
     notes = get_notes()
     id = get_free_id()
@@ -111,19 +147,48 @@ def add_note():
     save_notes()
     print(f'Заметка "{title}" добавлена.')
 
+#Удаляем заметку
+def delete_note():
+    notes = get_notes()
+    try:
+        id = int(input('Введите id заметки >> '))
+        notes = [note for note in notes if note.id != str(id)]
+        save_notes()
+        print('Заметка удалена')
+    except:
+        print('Некорректный id.')
 
 if __name__ == '__main__':
     greetings()
     while True:
         com = show_main_menu()
+        
         if com == 6:
             print('Завершение программы.')
             break
+        
         elif com == 1:
             print('Раздел: Управление заметками')
-            com = show_notes_menu()
-            if com == 8:
-                continue
+            while True:
+                com = show_notes_menu()
+                if com == 1:
+                    add_note()
+                elif com == 2:
+                    view_notes()
+                elif com == 3:
+                    view_note()
+                elif com == 4:
+                    update_note()
+                elif com == 5:
+                    delete_note()
+                elif com == 6:
+                    print('Команда в разработке')
+                elif com == 7:
+                    print('Команда в разработке')
+                elif com == 8:
+                    break
+                else:
+                    print('Выбрана неверная команда')
 
         else:
             print('Раздел в разработке. Выбери другой раздел.')
